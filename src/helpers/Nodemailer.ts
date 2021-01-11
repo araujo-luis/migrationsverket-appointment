91cbde13a4c6e5babfc20ff6e2ea86040a9d8270
Luis Araujo <luis_araujocaballero@hotmail.com>
2021-01-11 14:05:17 -0600

    Better console .log

--- src/helpers/Nodemailer.ts
+++ src/helpers/Nodemailer.ts
@@ -3,8 +3,9 @@ import dotenv from 'dotenv';
 import { AxiosResponse } from 'axios';
 import { createEmail } from '../utils/emailTemplate';
 import agencies from '../utils/agencies';
+import appoitmentsTypes from '../utils/appoitmentsTypes';
 import { EmailData } from '../utils/types';
-import { AGENCY_CODE, END_DATE, START_DATE } from '../config';
+import { AGENCY_CODE, END_DATE, START_DATE, APPOINTMENT_TYPE_CODE } from '../config';
 
 dotenv.config();
 const SENDER_EMAIL = process.env.SENDER_EMAIL;
@@ -22,12 +23,16 @@ const transporter = nodemailer.createTransport({
 export const proccesResponse = async (interval: NodeJS.Timeout, response:  AxiosResponse<any> ) => {
   let send = false;
   const emailData: EmailData = { agency: '', date: ''}
+  const agency = agencies.find(agency=> agency.code === AGENCY_CODE);
+  const appoitmentType = appoitmentsTypes.find(agency=> agency.code === APPOINTMENT_TYPE_CODE);
     console.log(`Request at: ${new Date()}`);
+    console.log(`Agency: ${agency?.text}`);
+    console.log(`Appointment type: ${appoitmentType?.name}`);
     console.log(`Appoitments avaiable between ${START_DATE} and ${END_DATE} -> ${response.data.length}`)
     if (response.data.length > 0){
       clearInterval(interval);
       send = true
-      emailData.agency = agencies.find(agency=> agency.code === AGENCY_CODE)?.text || ''
+      emailData.agency = agency?.text || ''
       emailData.date = `${START_DATE} - ${END_DATE}`;
     }
   if(send) await sendEmail(emailData);
